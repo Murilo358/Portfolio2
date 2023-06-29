87ab94b103ae1f844c8a67834f4d3b0fc41118b6
Murilo358 <murilobarbosa358@gmail.com>
2023-06-29 08:19:06 -0300

    adding form alert

--- src/Components/contactSection/contactSection.jsx
+++ src/Components/contactSection/contactSection.jsx
@@ -1,7 +1,48 @@
 import "./contactSection.scss";
+import { useState } from "react";
 import contactImg from "../../assets/Contact.svg";
 import Footer from "../Footer/Footer";
+import Swal from "sweetalert2";
+import { ImSpinner8 } from "react-icons/im";
 function contactSection() {
+  const [loading, setLoading] = useState(false);
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    setLoading(true);
+    const formData = new FormData(event.target);
+
+    try {
+      const response = await fetch(
+        "https://formsubmit.co/ajax/murilobarbosa358@gmail.com",
+        {
+          method: "POST",
+          body: formData,
+        }
+      );
+
+      document.getElementById("name").value = "";
+      document.getElementById("email").value = "";
+      document.getElementById("subject").value = "";
+      document.getElementById("message").value = "";
+
+      Swal.fire({
+        icon: "success",
+        color: "#d1d0cf",
+        title: "Email enviado com sucesso",
+        text: "Muito obrigado por entrar em contato",
+        background: "#1d1d1d",
+        showConfirmButton: false,
+        timer: 2500,
+      });
+    } catch (error) {
+      console.error("Erro:", error);
+    }
+
+    setLoading(false);
+  };
+
   return (
     <div className="section  contact-section-container">
       <h1>ENTRE EM CONTATO</h1>
@@ -13,13 +54,7 @@ function contactSection() {
           alt=""
         />
 
-        <form
-          className=""
-          action="https://formsubmit.co/murilobarbosa358@gmail.com"
-          method="POST"
-        >
-          {/* <input type="hidden" name="_captcha" value="false" />
-        <input type="hidden" name="_next" value="json" /> */}
+        <form onSubmit={handleSubmit}>
           <input
             type="text"
             id="name"
@@ -58,7 +93,7 @@ function contactSection() {
               role="status"
               aria-hidden="true"
             ></span>
-            <span>ENVIAR</span>
+            {loading ? <ImSpinner8 className="spin" /> : <span>ENVIAR</span>}
           </button>
         </form>
       </div>
